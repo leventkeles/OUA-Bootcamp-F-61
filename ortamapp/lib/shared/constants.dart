3ad07937b59cd0375b018da151338fa20ad7fff9
Levent Keles <levent@webtakibi.com>
2023-06-22 10:15:29 +0300

    proje dosyalarının eklenmesi

---
+++ ortamapp/lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:287385853570:android:e7a5e1306436ae2c1d5e9b";
+  static String apiKey = "AIzaSyDfB7lkK2GuyTckrOyjdLyzCTQIO7V0ByQ";
+  static String messagingSenderId = "287385853570";
+  static String projectId = "ortamapp-1777b";
+  final primaryColor = const Color(0xFFee7b64);
+}
